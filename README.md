35ac846d265eff450d84b107ef72482e8932810f
Fakhar Rana <fakharayub98@gmail.com>
2022-02-05 01:17:54 +0500

    Todo App

---
+++ README.md
@@ -0,0 +1,14 @@
+# Todo App
+
+## Description
+
+An application in which you can add, remove or edit your daily tasks. It can be a very useful app to maintain daily task list.
+
+## Technologies
+
+- JavaScript
+- React Native
+
+## Image
+
+![Todo App](TodoApp.PNG)
---
+++ components/ButtonComponent.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { TouchableOpacity, View, Text, StyleSheet } from 'react-native';
+
+const CustomButton = (props) => {
+  var btnColor;
+  if (props.disabled) btnColor = 'grey';
+  else btnColor = props.color != undefined ? props.color : 'blue';
+  return (
+    <TouchableOpacity
+      activeOpacity={0.5}
+      onPress={props.onPressEvent}
+      disabled={props.disabled}>
+      <View style={{ ...styles.buttonContainer, backgroundColor: btnColor }}>
+        <Text
+          style={{
+            fontSize: props.textSize,
+            color: props.textColor,
+          }}>
+          {props.text}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    paddingHorizontal: 20,
+    padding: 10,
+    borderRadius: 50,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+  },
+});
+
+export default CustomButton;
